dab715bfb0bcd3c4f2b2ac7689d2b44e4edff953
Nikhil Choudhary <inikhil001@gmail.com>
2022-06-03 11:11:11 +0530

    changes

--- encapsulation_Getters/nil_check.go
+++ encapsulation_Getters/nil_check.go
@@ -6,7 +6,7 @@ func main() {
 	//p := &Person{age: 25, name: "Nikhil"}
 	var p *Person
 	fmt.Println(p.getAge())
-	fmt.Println(p.age)
+	fmt.Println(p.age) // will give panic nil-pointer
 }
 
 type Person struct {
@@ -20,3 +20,11 @@ func (p *Person) getAge() int {
 	}
 	return 0
 }
+
+// same as above except return type
+// func (p *Person) getAge() (i int) {
+// 	if p != nil {
+// 		i = p.age
+// 	}
+// 	return
+// }
